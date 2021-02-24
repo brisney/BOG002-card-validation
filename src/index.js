205f991ac79ddc695b80208b00813dad70bf2275
brisney quiñonez <brisneyducuara@gmail.com>
2021-02-24 16:48:23 -0500

    en este cambio se agrego estilo al cambio de nombre

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
 
 console.log(validator);
+var boton= document.getElementById("sig");//aqui estoy declarando la variable boton leyendo el id del html que se llama nombreusuario
+  boton.addEventListener("click", saludousuario);//aqui estoy generando un evento mediante un click generando la funcion saludo usuario
+  function saludousuario() { //declaro la funcion que se va a llamar saludo usuario
+    var name= document.getElementById("nombreusuario").value;
+    document.write("Hola  "  + name +" " + "bienvenido" );
+  }
+  var botontarjeta= document.getElementById("mostrartarjeta");
+  botontarjeta.addEventListener("click", mensajenumerotarjeta);
+  function mensajenumerotarjeta() {
+    var numero= document.getElementById("numerotarjeta").value;
+    document.write("Tu numero de tarjeta es:  "  + numero +" " );
+}
