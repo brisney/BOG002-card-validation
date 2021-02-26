6417254e4e92476aedb51016897a8f984954fc76
brisney quiñonez <brisneyducuara@gmail.com>
2021-02-26 11:33:14 -0500

    se agrego estilos al html con campos para ingresar información

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import validator from './validator.js';
 
 console.log(validator);
-var boton= document.getElementById("sig");//aqui estoy declarando la variable boton leyendo el id del html que se llama nombreusuario
-  boton.addEventListener("click", saludousuario);//aqui estoy generando un evento mediante un click generando la funcion saludo usuario
 
-  //Esta es la primera función 
-  function saludousuario() { //declaro la funcion que se va a llamar saludo usuario
-    var name= document.getElementById("nombreusuario").value;
-    document.write("Hola  "  + name +" " );
-  
-  var botontarjeta= document.getElementById("mostrartarjeta");
-  botontarjeta.addEventListener("click", mensajenumerotarjeta);
-  // esta es la segunda función
-  function mensajenumerotarjeta() {
-    var numero= document.getElementById("numerotarjeta").value;
-    document.write("Tu numero de tarjeta es:  "  + numero +" " );
-}elem.style.display = "block"}
