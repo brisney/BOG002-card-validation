7f41eff66a8772b599df615c0ee55d72059db960
JessiJoya <jessica.joyaq@gmail.com>
2021-03-03 12:53:04 -0500

    prueba con el string mostrando el string en el console log

--- src/index.js
+++ src/index.js
@@ -4,14 +4,46 @@ console.log(validator);
 
 
 
-function cambiar () //declaran la función cambiar
+function cambiarHTML (numerodeti) //declaran la función cambiar
 { 
+    
     var nombre = document.getElementById("espacioparanombre").value //se esta declarando una variable que va a ser igual a lo que el usuario ingreso en espacio para nombre
-    var numerodeti = document.getElementById("espacioparaelnumtarjeta").value  //aqui se imprime hola más nombre
+    
+    console.log (typeof numerodeti)
 
     document.getElementById("formulario").innerHTML= "Hola " + nombre + " tu numero de tarjeta es " + numerodeti +"" ;
-}
+    var tamanostring  = numerodeti.length;
+        var cadenaInvertida = "";
+    
+        while (tamanostring>=0) {
+            cadenaInvertida = cadenaInvertida + numerodeti.charAt(tamanostring);
+            tamanostring--;
+          }
+    
+          console.log (cadenaInvertida);
+    }
+   
+
+
+
+// function validar(numerodeti) {
+//     var tamanostring  = numerodeti.length;
+//     var cadenaInvertida = "";
+
+//     while (tamanostring>=0) {
+//         cadenaInvertida = cadenaInvertida + numerodeti.charAt(tamanostring);
+//         tamanostring--;
+//       }
+
+//       console.log (cadenaInvertida);
+// }
 
 document.getElementById("validar").onclick = function (){ // se ejecuta hasta que se da click
-    cambiar();
+
+    var numerodeti = document.getElementById("espacioparaelnumtarjeta").value  //aqui se imprime hola más nombre
+    
+    cambiarHTML(numerodeti);
 }
+
+
+
