c746cfdad45bc2722ad0af9d14071268af6a06d5
JessiJoya <jessica.joyaq@gmail.com>
2021-03-03 09:59:31 -0500

    interactuamos ocn el JS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import validator from './validator.js';
 
 console.log(validator);
-function saludaralusuario (){ //eatoy declarando la función saludar al usuario
- const nombre= document.getelementbyid("espacioparanombre").value//se esta declarando una variable que va a ser igual a lo que el usuario ingreso en espacio para nombre
- document.getelementbyid("formulario").innerHTML="Hola " + nombre}//aqui se imprime hola más nombre
 
-document.getelementbyid("validar").onclick=function(){
-    saludaralusuario()
+
+
+function cambiar () //declaran la función cambiar
+{ 
+    var nombre = document.getElementById("espacioparanombre").value //se esta declarando una variable que va a ser igual a lo que el usuario ingreso en espacio para nombre
+    var numerodeti = document.getElementById("espacioparaelnumtarjeta").value  //aqui se imprime hola más nombre
+
+    document.getElementById("formulario").innerHTML= "Hola " + nombre + " tu numero de tarjeta es " + numerodeti +"" ;
+}
+
+document.getElementById("validar").onclick = function (){ // se ejecuta hasta que se da click
+    cambiar();
 }
