d4675424cdf0ca1d6fe66893183651bfe5b9c405
brisney quiñonez <brisneyducuara@gmail.com>
2021-03-03 09:31:44 -0500

    se probo coddigo usando h

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import validator from './validator.js';
 
 console.log(validator);
+function saludaralusuario (){ //eatoy declarando la función saludar al usuario
+ const nombre= document.getelementbyid("espacioparanombre").value//se esta declarando una variable que va a ser igual a lo que el usuario ingreso en espacio para nombre
+ document.getelementbyid("formulario").innerHTML="Hola " + nombre}//aqui se imprime hola más nombre
 
+document.getelementbyid("validar").onclick=function(){
+    saludaralusuario()
+}
