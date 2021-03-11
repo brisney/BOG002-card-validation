1fe441e5928220d23b1c6a339a3c448c6e2c4c6f
JessiJoya <jessica.joyaq@gmail.com>
2021-03-11 10:19:53 -0500

    ya remplazamos los nuemros y seleccionamos las posiciones pares

--- src/validator.js
+++ src/validator.js
@@ -2,18 +2,35 @@ const validator = {
   isvalid,
 };
 function isvalid(numerodetarjeta){
-  let tamanostring  = numerodetarjeta.length;
+  let tamanostring  = numerodetarjeta.length;// identificar cuantos caracteres tiene
+  console.log(tamanostring)
   let cadenaInvertida = "";
+  console.log(tamanostring)
 
-  while (tamanostring>=0) {
+// while que me permite tener el string de izquierda a derecha
+  while (tamanostring>=0) { 
       cadenaInvertida = cadenaInvertida + numerodetarjeta.charAt(tamanostring);
       tamanostring--;
      }
 
      console.log (cadenaInvertida);
      
-let arr = cadenaInvertida.split('');
-console.log(arr); 
+let arrayInvertida = cadenaInvertida.split(''); //variable que me almacena el string en un tipo array
+console.log(arrayInvertida); 
+
+
+// los for me ayudan a remplazar las posiciones pares por el numero multipliocado x2
+for (let i = 0; i < arrayInvertida.length; i++) {
+  for (let j     = 0; j < arrayInvertida[i].length; j++) {
+    if ( i%2 === 0) {
+      arrayInvertida[i] = (arrayInvertida[i])*2;
+      console.log(arrayInvertida);
+    }
+  }
+}
+console.log(arrayInvertida[2])  
+
+
 return true;
 }
 export default validator;
