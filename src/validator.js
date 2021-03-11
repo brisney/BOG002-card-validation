3ef44861cf87f9925698f841ee288c41944d7448
brisney quiñonez <brisneyducuara@gmail.com>
2021-03-10 20:37:50 -0500

    se agrego nuevas variables pero no se le han dado funcion

--- src/validator.js
+++ src/validator.js
@@ -14,6 +14,33 @@ function isvalid(numerodetarjeta){
      
 let arr = cadenaInvertida.split('');
 console.log(arr); 
+
+// var cadena = cadenainvertida;
+//   var longitud = tamanostring;
+//   var cifra = null;
+//   var cifra_cad=null;
+//   var suma=0;
+//   for (var i=0; i < longitud; i+=2){
+//     cifra = parseInt(cadena.charAt(i))*2;
+//     if (cifra > 9){ 
+//       cifra_cad = cifra.toString();
+//       console.log(cifra_cad+"esto es la variable cifra_cad");
+//       cifra = parseInt(cifra_cad.charAt(0)) + 
+//  parseInt(cifra_cad.charAt(1));
+//     }
+//     console.log(cifra+"esto es el valor de cifra")
+//     suma+=cifra;
+//   }
+//   for (var i=1; i < longitud; i+=2){
+//     suma += parseInt(cadena.charAt(i));
+//   }
+   
+//   if ((suma % 10) == 0){ 
+//    alert("Número de tarjeta correcto");
+//   } else {
+//    alert("El número de tarjeta no es válido");
+//   }
+
 return true;
 }
 export default validator;
