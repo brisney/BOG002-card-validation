6170b823921749b199597999b9f3874a1615b2b3
brisney quiñonez <brisneyducuara@gmail.com>
2021-05-10 11:24:05 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,81 @@
+//se llama al objeto de validación de la tarjeta que se encuentra en validator.js no estamos haciendo nada con esto aun 
+import validator from './validator.js';
+console.log(validator);
+
+//Declaración de variables que me permiten ingresar a los campos con los que interactua el usuario 
+// declara variables es como bautizar en JS 
+
+//var nombredelavariablenJS = (se va a declarar como) la información que encuentre en el HTML con el id que se pone en comillas
+let campoformulario = document.getElementById("cuadrovalidar");
+let inputnumerotarjeta = document.getElementById("espacioparaelnumtarjeta");
+let inputnombredeusuario = document.getElementById("espacioparanombre");
+let inputnombredeusuariofecha = document.getElementById("espaciofechavencimiento");
+let enunciadoInvalido= document.getElementById("enunciadoForm");
+let inputbotonvalidar = document.getElementById("validar");
+let inputbotonreset = document.getElementById("resetear");
+
+
+
+                    //los cambios que estamos haciendo sobre todo el documento 
+
+//ubicandonos en un campo .evento = le decimos que nos ejecute una funcion que esta definida dentro de los corchetes
+    inputbotonvalidar.onclick = function evitarquecambiesiestavacio () { // se ejecuta hasta que se da click
+     
+    let numerodeti = inputnumerotarjeta.value;  
+    let nombredeus = inputnombredeusuario.value;
+
+    // la funcion if esta diciendo si (variablenumero de tarjeta es diferente de vacio ejecute lo que esta dentro de los corchetes {}
+    if (numerodeti != "" && nombredeus != "" ){ 
+        
+        cambiarHTML(numerodeti);
+        }
+        
+    }
+
+//se hace una funcion que imprime los datos ingresados por el usuario en otro cmapo 
+function cambiarHTML (numerodeti) {//declaran la función en la vamos a mostrar el texto 
+    var nombre = inputnombredeusuario.value; //usando los datos de espacioparanombre y convirtiendolos en la variable nombre JS
+    var fechaven = inputnombredeusuariofecha.value;
+    let resultadovalidacion =  validator.isvalid(numerodeti);
+    let stringmaskif =  validator.maskify(numerodeti);
+
+    if (resultadovalidacion === true ){
+      
+       resultadovalidacion = "valida";
+       
+       campoformulario.innerHTML= "Hola " + nombre + " tu número de tarjeta finalizada en " + stringmaskif + " con fecha de vencimiento " + fechaven  + " es " + resultadovalidacion;
+    } else {
+        resultadovalidacion = "invalida";
+        enunciadoInvalido.innerHTML= "Tu tarjeta es " +resultadovalidacion + " por favor consigna de nuevo los datos ";
+    }
+
+//    if (resultadovalidacion === false ){
+//     resultadovalidacion = "invalida";
+//     campoformulario.innerHTML=  " Tu tarjeta es  " + resultadovalidacion + " por favor ingresa de nuevo ";//imprimiendo el mensjaes con los datos
+//    }else {
+//    resultadovalidacion = "valida"
+   
+//    campoformulario.innerHTML= "Hola " + nombre + " tu número de tarjeta finalizada en " + stringmaskif + " con fecha de vencimiento " + fechaven  + " es " + resultadovalidacion;//imprimiendo el mensjaes 
+// }
+    
+   
+}
+  
+ //evento key up para ingresar el nombre en mayusculas
+ inputnombredeusuario.addEventListener("keyup",Restringirnombre );
+
+function Restringirnombre() {
+   // declaro la variable nombre en JS que toma la información de espacio para nombre
+      inputnombredeusuario.value = inputnombredeusuario.value.toUpperCase(); //tomo el valor del nombre y a cada caracter le pongo mayusculas 
+}
+ 
+//evento key up para ingresar solo caracteres numericos en el campo de la tarjeta
+inputnumerotarjeta.addEventListener("keyup",restringirnumero );
+
+function restringirnumero() {
+        inputnumerotarjeta.value= inputnumerotarjeta.value.replace(/\D+/g, '').replace(/\s/g, '').trim ();
+}
+
+
+
+
---
+++ validator.js
@@ -0,0 +1,58 @@
+const validator = {
+  isvalid,
+  maskify,
+};
+function isvalid(numerodetarjeta) {
+  let tamanostring = numerodetarjeta.length;// identificar cuantos caracteres tiene
+  console.log(tamanostring)
+  let cadenaInvertida = "";//es un campo vacio
+  console.log(tamanostring)
+
+  // while que me permite tener el string de izquierda a derecha
+  while (tamanostring >= 0) {
+    //estamos convirtiendo en un array separando cada uno de los caracteres
+    cadenaInvertida = cadenaInvertida + numerodetarjeta.charAt(tamanostring);
+    tamanostring--;//es el contador del ciclo y esta disminuyendo cada vez que se ejecute
+  }
+
+  console.log(cadenaInvertida);
+
+  let arrayInvertida = cadenaInvertida.split(''); //variable que me almacena el string en un tipo array ""
+  console.log(arrayInvertida);
+
+  for (let i = 0; i < arrayInvertida.length; i++) {//
+    if (i % 2 != 0) {
+      arrayInvertida[i] = (arrayInvertida[i]) * 2;
+      console.log(arrayInvertida);
+    }
+    let valor = arrayInvertida[i];
+    if (valor >= 10) {
+      let num1 = Number(valor.toString().split('')[0]);
+      let num2 = Number(valor.toString().split('')[1]);
+      let sumaNume = num1 + num2;
+      arrayInvertida[i] = sumaNume;;
+    } else if (i % 2 === 0) {
+      arrayInvertida[i] = (arrayInvertida[i] * 1);
+      console.log(arrayInvertida);
+
+    }
+  }
+
+  let totalstring = arrayInvertida.reduce((a, b) => a + b, 0);
+  console.log(totalstring);
+  if ((totalstring % 10) === 0) {
+    console.log("deberia retornar true");
+    return true;
+  } else {
+    console.log("deberia retornar false");
+    return false;
+  }
+}
+
+function maskify(numtarjeta) {
+  let cadenastring = numtarjeta.replace(/.(?=.{4})/g, "#");
+  console.log(cadenastring);
+  return cadenastring;
+}
+
+export default validator;
